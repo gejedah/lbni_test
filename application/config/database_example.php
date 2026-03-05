4c703fa06fa3ba6ced80df8f9c1fc352632d97a9
Vinzh <kevin.zhong@gmail.com>
2026-03-05 09:21:51 +0700

    gitignore

---
+++ application/config/database_example.php
@@ -0,0 +1,5 @@
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'debian';'
+$db['default']['password'] = '';
+$db['default']['database'] = 'lbni_testing';
+$db['default']['dbdriver'] = 'mysqli';
